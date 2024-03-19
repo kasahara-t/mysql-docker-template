aec8e1028e680bf12af3a7fc254431b472e04b68
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-03-19 12:43:32 +0900

    fix: :bug: mysqlのヘルスチェックを修正

--- docker/migrator/migrate.sh
+++ docker/migrator/migrate.sh
@@ -18,20 +18,5 @@ if [ -n "$MYSQL_DATABASE_FILE" ] && [ -f "$MYSQL_DATABASE_FILE" ]; then
     DATABASE=$(cat "$MYSQL_DATABASE_FILE")
 fi
 
-# MySQLに接続確認
-echo "Waiting for MySQL to start..."
-retries=25
-tries=0
-until mysql -h "$HOST" -P "$PORT" -u "$USER" -p"$PASSWORD" -e "show databases;" &> /dev/null; do
-    if [ "$tries" -eq "$retries" ]; then
-        >&2 echo "Failed to connect to MySQL after $retries retries - exiting"
-        exit 1
-    fi
-    >&2 echo "MySQL is unavailable - sleeping"
-    sleep 1
-    tries=$((tries + 1))
-done
-echo "MySQL is up - executing command"
-
 # マイグレーションの実行
 migrate -path /app/histories -database "mysql://${USER}:${PASSWORD}@tcp(${HOST}:${PORT})/${DATABASE}?multiStatements=true" up
